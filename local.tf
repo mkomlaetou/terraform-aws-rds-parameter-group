2d0ba10fa3b04a6df8a11fd46f6ee69d35841302
mkomlaetou <m.komlaetou@gmail.com>
2024-06-18 17:17:45 +0000

    rename variables and update readme

--- local.tf
+++ local.tf
@@ -1,4 +1,4 @@
 
 locals {
-  cluster_pg_parameters = merge(var.cluster_pg_parameters, var.db_pg_parameters)
+  cluster_pg_parameters = merge(var.cluster_pg_parameters, var.instance_pg_parameters)
 }
--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
+###############################
+# CREATE DB CLUSTER AND INSTANCE PARAMETER GROUP
+##############################
 
-// CREATE A CLUSTER PARAMETER GROUP
+// create a cluster parameter group
 resource "aws_rds_cluster_parameter_group" "cluster_pg" {
   count       = var.create_cluster_pg ? 1 : 0
   name        = var.pg_info["name"]
@@ -17,15 +20,15 @@ resource "aws_rds_cluster_parameter_group" "cluster_pg" {
 }
 
 
-// CREATE A DB INSTANCE  PARAMETER GROUP
-resource "aws_db_parameter_group" "db_pg" {
-  count       = var.create_db_pg ? 1 : 0
+// create an instance parameter group
+resource "aws_db_parameter_group" "instance_pg" {
+  count       = var.create_instance_pg ? 1 : 0
   name        = var.pg_info["name"]
   family      = var.pg_info["family"]
-  description = var.pg_info["description"] == "" ? "${var.pg_info["name"]} Db PG" : var.pg_info["description"]
+  description = var.pg_info["description"] == "" ? "${var.pg_info["name"]} Instance PG" : var.pg_info["description"]
 
   dynamic "parameter" {
-    for_each = var.db_pg_parameters
+    for_each = var.instance_pg_parameters
     content {
       name         = parameter.value["name"]
       value        = parameter.value["value"]
